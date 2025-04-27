1f14c3220ad39e119e6b63fb37050ab916cc3337
Ivan Bozovic <ivan.bozovic02@gmail.com>
2025-04-27 23:00:39 +0200

    Rename Camera to Drone

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Camera : MonoBehaviour
+public class Drone : MonoBehaviour
 {
     [SerializeField] private float lookSensitivity;
     [SerializeField] private Vector2 pitchRange;
