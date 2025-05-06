c6bedbf658ed42ed01a37a94c12418e23875a532
Ivan Bozovic <ivan.bozovic02@gmail.com>
2025-05-07 00:47:15 +0200

    EventBus

---
+++ Assets/Core/Scripts/Infrastructure/EventBus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Infrastructure
+{
+    public static class EventBus
+    {
+        private static readonly Dictionary<Type, List<Delegate>> Subscribers = new();
+
+        public static void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : IEvent
+        {
+            var type = typeof(TEvent);
+
+            if (!Subscribers.ContainsKey(type))
+            {
+                Subscribers[type] = new List<Delegate>();
+            }
+
+            Subscribers[type].Add(handler);
+        }
+
+        public static void Unsubscribe<TEvent>(Action<TEvent> handler) where TEvent : IEvent
+        {
+            var type = typeof(TEvent);
+
+            if (Subscribers.ContainsKey(type))
+            {
+                Subscribers[type].Remove(handler);
+                Subscribers.Remove(type);
+            }
+        }
+
+        public static void Notify<TEvent>(TEvent notifiedEvent) where TEvent : IEvent
+        {
+            var type = notifiedEvent.GetType();
+
+            if (Subscribers.TryGetValue(type, out var callbacks))
+            {
+                foreach (var callback in callbacks)
+                {
+                    ((Action<TEvent>)callback)?.Invoke(notifiedEvent);
+                }
+            }
+        }
+    }
+}
