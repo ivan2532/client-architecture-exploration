883782e7483b5b3355ba370db5b385e9741ab4d6
Ivan Bozovic <ivan.bozovic02@gmail.com>
2025-05-07 02:18:35 +0200

    ViewModel abstraction

--- Assets/Core/Scripts/View/ViewBase.cs
+++ Assets/Core/Scripts/View/ViewBase.cs
@@ -1,8 +1,26 @@
-﻿using UnityEngine;
+﻿using Core.ViewModel;
+using UnityEngine;
 
 namespace Core.View
 {
     public abstract class ViewBase : MonoBehaviour
     {
+    }
+
+    public abstract class ViewBase<TViewModel> : ViewBase where TViewModel : IViewModel
+    {
+        public TViewModel ViewModel { get; protected set; }
+
+        protected virtual void Awake()
+        {
+            InitializeViewModel();
+        }
+
+        protected abstract void InitializeViewModel();
+
+        public virtual void UpdateViewModel(TViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
     }
 }
