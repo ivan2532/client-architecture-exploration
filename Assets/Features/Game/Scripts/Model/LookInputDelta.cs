1ba3b0a0b65c621ae859526f142a243c6975d926
Ivan Bozovic <ivan.bozovic02@gmail.com>
2025-05-07 13:39:44 +0200

    Remove accidental implementation of IEvenet in LookInputDelta

--- Assets/Features/Game/Scripts/Model/LookInputDelta.cs
+++ Assets/Features/Game/Scripts/Model/LookInputDelta.cs
@@ -1,6 +1,4 @@
-﻿using Core.Infrastructure;
-
-namespace Features.Game.Model
+﻿namespace Features.Game.Model
 {
-    public record LookInputDelta(float X, float Y) : IEvent;
+    public record LookInputDelta(float X, float Y);
 }
