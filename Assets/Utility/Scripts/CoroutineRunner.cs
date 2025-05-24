99886e511af97e91e2d482aa6839f71553d393c6
Ivan Bozovic <ivan.bozovic02@gmail.com>
2025-05-24 14:47:41 +0200

    Adjust CoroutineRunner namespace, create ICoroutineRunner

---
+++ Assets/Utility/Scripts/CoroutineRunner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Utility
+{
+    public class CoroutineRunner : MonoBehaviour, ICoroutineRunner
+    {
+        public void Run(IEnumerator coroutine)
+        {
+            StartCoroutine(coroutine);
+        }
+    }
+
+    public interface ICoroutineRunner
+    {
+        public void Run(IEnumerator coroutine);
+    }
+}
