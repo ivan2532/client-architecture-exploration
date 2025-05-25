9b23b80d5cd71275d0cfce3c91f451434bb768cf
Ivan Bozovic <ivan.bozovic02@gmail.com>
2025-05-25 22:54:05 +0200

    Domain context base class

---
+++ Assets/Core/DomainContextSystem/DomainContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.DomainContextSystem
+{
+    public abstract class DomainContext : ScriptableObject, IDisposable
+    {
+        public void Initialize()
+        {
+            CreateInputAdapters();
+            CreateOutputAdapters();
+            CreateService();
+            ResolveInternalCircularDependencies();
+        }
+
+        public abstract void Dispose();
+
+        public abstract void ResolveExternalCircularDependencies();
+
+        protected abstract void CreateInputAdapters();
+
+        protected abstract void CreateOutputAdapters();
+
+        protected abstract void CreateService();
+
+        protected abstract void ResolveInternalCircularDependencies();
+    }
+}
